71c294ac41be4fd8e16febc47cb3e78bd1f33118
Hichxm <hicham.slimani.fr@gmail.com>
2026-07-02 22:59:28 +0200

    back: wanted error

--- tests/HyperLogLogTest.php
+++ tests/HyperLogLogTest.php
@@ -166,7 +166,5 @@ class HyperLogLogTest extends TestCase
             $hll->estimateUsingLargeCardinalitiesApproach($rawEstimate),
             0.001
         );
-
-        $this->assertTrue(false);
     }
 }
